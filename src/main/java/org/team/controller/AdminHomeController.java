c5a7fa197025cb34df4eb877c5763d5624eb5415
toobi <127570017+Teletobimy@users.noreply.github.com>
2023-12-07 16:49:11 +0900

    Revert "중복된 파일 삭제"

---
+++ src/main/java/org/team/controller/AdminHomeController.java
@@ -0,0 +1,19 @@
+package org.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/admin/")
+@AllArgsConstructor
+@Log4j
+public class AdminHomeController {
+	@GetMapping(value = "/home")
+	public void home() {
+		log.info("admin home");
+	}
+}
